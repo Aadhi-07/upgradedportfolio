5efca912a7c0c464ba78913c65b5aca6cfa797df
Aadhi-07 <aadhithiyajayaraman@gmail.com>
2025-12-15 20:38:28 +0530

    made changes

--- remix.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  serverBuildPath: "build/server/index.js",
-  assetsBuildDirectory: "build/client",
-  publicPath: "/build/",
-  serverModuleFormat: "esm",
-  serverPlatform: "node"
-};
