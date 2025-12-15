dc7d31373f3adf775ccf8521d5fafaf502cfbbf0
Aadhi-07 <aadhithiyajayaraman@gmail.com>
2025-12-15 22:20:08 +0530

    updates madE

--- build/server/index.js
+++ build/server/index.js
@@ -7,6 +7,13 @@ import "@remix-run/cloudflare";
 import "react";
 import "framer-motion";
 import "@mdx-js/react";
+import { createRequestHandler } from "@remix-run/node";
+import * as build from "../build/server/index.js";
+
+export default createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV
+});
 export {
   s as assets,
   f as assetsBuildDirectory,
