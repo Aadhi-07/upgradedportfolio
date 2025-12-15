3cf001407e047ec4b1058ec19da8e5cd1af8c963
Aadhi-07 <aadhithiyajayaraman@gmail.com>
2025-12-15 20:20:59 +0530

    Add Remix config for Node runtime

---
+++ remix.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  serverBuildPath: "build/server/index.js",
+  assetsBuildDirectory: "build/client",
+  publicPath: "/build/",
+  serverModuleFormat: "esm",
+  serverPlatform: "node"
+};
