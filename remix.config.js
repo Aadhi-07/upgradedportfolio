f9f47892c2ca5626d5223ec44ef141f8051cc9a9
Aadhi-07 <aadhithiyajayaraman@gmail.com>
2025-12-16 15:28:27 +0530

    fix remix routing

--- remix.config.js
+++ remix.config.js
@@ -2,5 +2,5 @@ export default {
   appDirectory: "app",
   ignoredRouteFiles: ["**/.*"],
   serverBuildTarget: "node-cjs", // build for Node
-  server: "./index.js",     // Remix will output here
+  server: "index.js",     // Remix will output here
 };
