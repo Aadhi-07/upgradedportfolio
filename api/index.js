d75ab1e2b76589caade605a11af5799de5ca2334
Aadhi-07 <aadhithiyajayaraman@gmail.com>
2025-12-21 10:03:21 +0530

    fix remix routing

--- api/index.js
+++
@@ -1,7 +0,0 @@
-import { createRequestHandler } from "@remix-run/vercel";
-import * as build from "../build/index.js"; // path to Remix build
-
-export const handler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
